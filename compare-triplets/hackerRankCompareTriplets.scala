e792438673ede1f1a8cce6f213ecaa626e2b493a
David Masterson <david73180@gmail.com>
2020-12-09 05:44:50 -0600

    working new compare triplets problem set

---
+++ compare-triplets/hackerRankCompareTriplets.scala
@@ -0,0 +1,16 @@
+object Solution {
+
+    // Complete the compareTriplets function below.
+    def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
+        var alicePoints = 0
+        var bobsPoints = 0
+        var iteration = 0
+        var answer: Array[Int]= new Array[Int](2)
+        var answer = (alicePoints,bobsPoints) 
+        
+        for (i <- a )
+            println(i)
+        return answer
+
+    }
+}
